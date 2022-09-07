b0b989345425e017fb857d10246d4ef6af8b2d1a
ihandmine <handmine@outlook.com>
2022-09-07 11:57:40 +0800

    chore: setup 1.0.9

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ DESCRIPTION = "fake pc or app browser useragent, anti useragent, and other aweso
 URL = "https://github.com/ihandmine/anti-useragent"
 AUTHOR = "handmine"
 AUTHOR_EMAIL = "handmine@outlook.com"
-VERSION = "1.0.8"
+VERSION = "1.0.9"
 LICENSE = "MIT"
 REQUIRES_PYTHON = ">=3.7.0"
 
