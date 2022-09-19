b0e87c1695f28257bce150bdf1d2cf3c71233aa2
ihandmine <handmine@outlook.com>
2022-09-19 15:00:03 +0800

    1.0.10

--- anti_useragent/__init__.py
+++ anti_useragent/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 
 from anti_useragent.useragent.ua import AntiUserAgent, UserAgent
 
-__version__ = '1.0.9'
+__version__ = '1.0.10'
 
 VERSION = __version__
 
